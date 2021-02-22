0166dcfe44cc6db1154465541ef686d99b927be9
rychao <rychao@gmail.com>
2021-02-21 21:18:48 -0800

    update app

--- App.js
+++ App.js
@@ -1,96 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, KeyboardAvoidingView, Platform, TouchableOpacity, TextInput } from 'react-native';
-import Todo from './components/todo'
+import React, {Component} from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import ToDo from "./screens/ToDo"
+import Welcome from "./screens/welcome"
 
-export default function App() {
+const Tab = createBottomTabNavigator();
 
-  const [task, setTask] = useState();
-  const [taskItems, setTaskItems] = useState([]);
-
-  const handleAddTask = () => {
-    Keyboard.dismiss();
-    console.log(task)
-
-  }
-
-  const completeTask = () => {
-    let itemsCopy = [...taskItems];
-    itemsCopy.splice(index,1);
-    setTaskItems(itemsCopy);
-  }
-
-  return (
-    <View style={styles.container}>
-    {/*Tasks*/}
-    <View style={styles.taskWrapper}>
-    <Text style={styles.sectionTitle}>UC Merced Homework</Text>
-        <View style={styles.items}></View>
-    {
-    taskItems.map((item, index) => {
-      return(<TouchableOpacity key={index} on press={() => completeTask(index)}>
-        <Todo text={item}/>
-      </TouchableOpacity>)
-    })
-  }
-        {/*todos*/}
-      </View>
-      <KeyboardAvoidingView
-      behavior ={Platform.OS === 'ios' ? 'padding': 'height'}
-      style={styles.writeTaskWrapper}>
-        <TextInput style={styles.input} placeholder={"Write a Task"} value={task} onChangeText={text => setTask(text)}/>
-        <TouchableOpacity onPress={() => handleAddTask()}>
-          <View style={styles.addWrapper}>
-            <Text style={styles.addText}>+</Text>
-          </View>
-        </TouchableOpacity>
-      </KeyboardAvoidingView>
-    </View>
-  );
+export default function App(){
+    return(
+        <NavigationContainer>
+            <Tab.Navigator>
+                <Tab.Screen name="Welcome" component={Welcome}/>
+                <Tab.Screen name="Tasks" component={ToDo}/>
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#E8EAED',
-  },
-  tasksWrapper: {
-    paddingTop: 80,
-    paddingHorizontal: 20,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: 'bold'
-  },
-  items: {
-    marginTop: 30,
-  },
-  writeTaskWrapper: {
-    position: 'absolute',
-    bottom: 60,
-    width: '100%',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center'
-  },
-  input: {
-    paddingVertical: 15,
-    paddingHorizontal: 15,
-    backgroundColor: '#FFF',
-    borderRadius: 60,
-    borderColor: '#C0C0C0',
-    borderWidth: 1,
-    width: 250,
-  },
-  addWrapper: {
-    width: 60,
-    height: 60,
-    backgroundColor: '#FFF',
-    borderRadius: 60,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderColor: '#C0C0C0',
-    borderWidth: 1,
-  },
-  addText: {},
-});
